faa5068392c3ffdd64f2fd67b837e5ca4d138782
dengchang <1332524093@qq.com>
2019-12-24 18:55:40 +0800

    完善课程和课时功能

--- server/apps/admin/src/episcodes/episcodes.controller.ts
+++ server/apps/admin/src/episcodes/episcodes.controller.ts
@@ -14,15 +14,22 @@ import { Crud } from 'nestjs-mongoose-crud';
 export class EpiscodesController {
     constructor(
         @InjectModel(Episode) private readonly model: ReturnModelType<typeof Episode>,
+        @InjectModel(Course) private readonly courseModel: ReturnModelType<typeof Course>,
     ) { }
 
     @Get('option')
-    option() {
+    async option() {
+        const courses = (await this.courseModel.find()).map(v => ({
+            label: v.name,
+            value: v._id,
+        }));
         return {
             title: '课时管理',
+            translate: false,
             column: [
-                { prop: 'name', label: '课时名称' },
-                // { prop: 'cover', label: '课程封面图' },
+                { prop: 'course', label: '所属课程', type: 'select', row: true, dicData: courses },
+                { prop: 'name', label: '课时名称', span: 24 },
+                { prop: 'file', label: '视频文件', listType: 'picture-img', span: 24, type: 'upload', action: '/upload', width: 120 },
             ],
         };
     }
--- server/libs/db/src/models/course.model.ts
+++ server/libs/db/src/models/course.model.ts
@@ -1,21 +1,22 @@
-import { prop, modelOptions, arrayProp, Ref } from '@typegoose/typegoose'
-import { ApiModelProperty } from '@nestjs/swagger'
-import { Episode } from './episode.model'
+import { prop, modelOptions, arrayProp, Ref } from '@typegoose/typegoose';
+import { ApiModelProperty } from '@nestjs/swagger';
+import { Episode } from './episode.model';
 
 @modelOptions({
   schemaOptions: {
-    timestamps: true
-  }
+    timestamps: true,
+  },
 })
 export class Course {
   @ApiModelProperty({ description: '课程名称' })
   @prop()
-  name: string
+  name: string;
 
   @ApiModelProperty({ description: '封面图' })
   @prop()
-  cover: string
+  cover: string;
 
-  @arrayProp({ itemsRef: 'Episode' })
-  episodes: Ref<Episode>[]
+/*   @arrayProp({ itemsRef: 'Episode' })
+  // tslint:disable-next-line: array-type
+  episodes: Ref<Episode>[]; */
 }
